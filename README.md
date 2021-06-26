6857ab55f8074005642f70df6ae2ad16e80f66b2
Andrea Miglietta <andrea.miglietta@protonmail.com>
2021-06-26 17:23:31 +0200

    Add update-oc-client.sh

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository collect scripts and utils that:
 ### Collection
 - src/bash/bin/[approve-ocp-csr.sh](/src/bash/bin/approve-ocp-csr.sh): Approve OpenShift CSR requests.
 - src/bash/bin/[switch-kube-context_rofi.sh](src/bash/bin/switch-kube-context_rofi.sh): Switch Kubernetes/OpenShift context (designed for i3wm+rofi).
+- src/bash/bin/[update-oc-client.sh](src/bash/bin/update-oc-client.sh): Update the user's oc client or install it if not present. 
 - src/bash/func/[switch-kube-context_fzf.sh](src/bash/func/switch-kube-context_fzf.sh): Switch Kubernetes/OpenShift context from terminal.
 - src/bash/func/[quay-utils.sh](src/bash/func/quay-utils.sh): Collection of oc commands useful to setup Quay for lazy people.
 - src/bash/prompt/[k8s-prompt.sh](src/bash/prompt/k8s-prompt.sh): Simple prompt function to customize PS1 for K8S/OCP.
-
---
+++ src/bash/bin/update-oc-client.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function update_oc_client() {
+
+  local BIN_DIR="$HOME/.local/bin"
+  local OC_ARCHIVE="/tmp/openshift-client-linux.tar.gz"
+
+  local OC_ARCHIVE_URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux.tar.gz"
+  local OC_SHA256_URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/sha256sum.txt"
+
+  echo "Downloading the archive into $OC_ARCHIVE.."
+  curl --silent --show-error --output "$OC_ARCHIVE" "$OC_ARCHIVE_URL"
+
+  local RSHA="$(curl -s $OC_SHA256_URL | awk '/openshift-client-linux/ { print $1 }')"
+  local LSHA="$(sha256sum $OC_ARCHIVE | awk '{ print $1 }')"
+
+  if [ "$RSHA" == "$LSHA" ]; then
+    echo -n "Check SHA256: good. "
+  else
+    echo "Check SHA256: wrong, abort."
+    return
+  fi
+
+  if [ ! -d "$BIN_DIR" ]; then
+    mkdir -p "$BIN_DIR";
+  fi
+  tar xf "$OC_ARCHIVE" -C "$BIN_DIR" oc kubectl
+  echo "Done."
+}
+
+# main
+if which curl 1>/dev/null 2>&1; then
+  update_oc_client
+else
+  echo "$(basename $0): curl command not found"
+fi
+
